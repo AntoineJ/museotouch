517496324c7cb4f3602ce9f0357528596fc19505
Louis <louisbeseme@gmail.com>
2013-06-25 15:00:26 +0200

    work in progress

--- app/expos/41/data/__init__.py
+++ app/expos/41/data/__init__.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
+
 from kivy.properties import StringProperty, ObjectProperty, NumericProperty, \
         BooleanProperty, ListProperty
 
 
-from os.path import join
+from os.path import join, isfile, basename, getsize
 from glob import glob
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
@@ -14,15 +16,166 @@ from museolib.widgets.keywords import Keywords
 from museolib.widgets.slider import SizeSlider
 from museolib.widgets.basket import Basket
 from kivy.utils import platform
+from random import choice, randint, sample, shuffle
+from kivy.animation import Animation
 
 
 class QuizzItem(Scatter):
-    item = ObjectProperty()
-    image = StringProperty('')
-    question = StringProperty('')
-    bonneReponse = StringProperty('')
-    mauvaiseReponse = StringProperty('')
 
+    app = ObjectProperty()
+
+    # Item associé à la question
+    item = ObjectProperty()
+
+    # Permet d'avoir une position aléatoire entre les deux réponses
+    position = NumericProperty(0)
+
+    # Permet de savoir si on a bien répondu
+    bonneReponse = BooleanProperty(False)
+
+    # Permet de savoir si on est en phase de correction
+    correction = BooleanProperty(False)
+
+    # Liste d'id de question
+    ordreQuestion = ListProperty([])
+
+    numeroQuestion = NumericProperty(0)
+
+    medias = ListProperty(None)
+
+    # image = StringProperty('')
+    # question = StringProperty('')
+    # bonneReponse = StringProperty('')
+    # mauvaiseReponse = StringProperty('')
+
+    def __init__(self, **kwargs):
+        
+        super(QuizzItem, self).__init__(**kwargs)
+
+        # Definition de l'ordre des question. A revoir pour le mode 2 joueurs
+        self.ordreQuestion = sample(range(len(self.app.db.items)), min(5,len(self.app.db.items)))
+
+        self.item = self.app.db.items[self.ordreQuestion[self.numeroQuestion]]
+
+        # Position aléatoire des réponses
+        self.position = choice([0,1])   
+        if self.position:
+            self.btnBonneReponse.y = 47
+        else:
+            self.btnMauvaiseReponse.y = 47
+
+
+        # Ajout des images de bonne/mauvaise reponse
+        for filedata in self.item.data:
+            if self.item.data.index(filedata) == 0:
+                continue
+            else:
+                fileurl = filedata['fichier']
+                filename = basename(fileurl)
+                filepath = join(self.app.expo_dir, 'otherfiles', filename)
+                if isfile(filepath) and getsize(filepath) > 0:
+                    self.medias.append(filepath)
+
+
+        self.rebuild()
+
+
+        self.btnMauvaiseReponse.bind(on_release= self.do_mauvaise_reponse)
+        self.btnBonneReponse.bind(on_release= self.do_bonne_reponse)
+        self.btnContinuez.bind(on_release= self.do_continue)
+
+    def do_bonne_reponse(self, kwargs):
+        if self.correction:
+            return
+
+        self.bonneReponse = True
+        self.correction = True
+        self.transform_ui()
+
+
+    def do_mauvaise_reponse(self, kwargs):
+        if self.correction:
+            return
+
+        self.bonneReponse = False
+        self.correction = True
+        self.transform_ui()
+
+    def do_continue(self, kwargs):
+        if not self.correction:
+            return
+
+        self.numeroQuestion += 1
+
+        if self.numeroQuestion >= len(self.app.db.items):
+            pass 
+        else:
+            self.item = self.app.db.items[self.ordreQuestion[self.numeroQuestion]]
+            self.rebuild()
+
+    def transform_ui(self):
+
+        anim = Animation(size=(230,400), d=0.2)
+        anim.start(self)
+
+
+        self.labelTitre.pos = (10,370)
+        if self.bonneReponse:
+            self.labelTitre.text = 'BONNE REPONSE !'
+            self.labelReponse.text = self.item['description']
+            if len(self.item['data']) > 1:
+                pass
+        else:
+            self.labelTitre.text = 'MAUVAISE REPONSE ...'
+            self.labelReponse.text = self.item['description2']
+            if len(self.item['data']) > 1:
+                pass
+
+        anim2 = Animation(y=340, d=.2)
+        anim2.start(self.btnBonneReponse)
+
+        #TODO: dessiner la ligne en haut
+
+        anim3 = Animation(size= (210,210), y=120, x=10, d=.2)
+        anim3.start(self.photo)
+
+        self.btnContinuez.opacity = 1
+        self.btnContinuez.y = 10
+
+        # self.btnBonneReponse.opacity = 0
+        self.btnMauvaiseReponse.opacity = 0
+
+
+
+    def rebuild(self):
+
+
+        self.labelTitre.text = self.item['nom']
+        self.btnMauvaiseReponse.text = self.item['freefield']
+        self.btnBonneReponse.text = self.item['orig_geo_prec']
+        self.photo.source= self.item.filename
+        self.labelReponse.text = ''
+        # self.labelMauvaiseReponse.opacity = 0
+
+        self.position = choice([0,1])
+        self.bonneReponse = False
+        self.correction = False
+        self.size = 230,354
+        self.btnContinuez.opacity = 0
+        self.btnContinuez.pos = 10,100
+        self.btnMauvaiseReponse.opacity = 1
+        self.btnMauvaiseReponse.y = 10
+        self.btnBonneReponse.y = 10
+
+        if self.position:
+            self.btnBonneReponse.y = 47
+        else:
+            self.btnMauvaiseReponse.y = 47
+
+        self.photo.size = 230,230
+        self.photo.pos = 0,124
+
+        self.labelTitre.y = 90
 
 
 
@@ -36,9 +189,7 @@ def build(app):
     # Our root widget
     root = FloatLayout()
 
-    question = QuizzItem(
-        app=app,
-        item = app.db.items[0])
+    question = QuizzItem(app=app)
 
     root.add_widget(question)
 
@@ -47,10 +198,7 @@ def build(app):
 
     # -------------------------------------------------------------------------
     # Add a date slider to our root widget.
-    print '***********************'
-    print dir(app.db.items[0].medias)
-    print app.db.items
-    print '***********************'
+
 
     # -------------------------------------------------------------------------
     # Create a basket widget
