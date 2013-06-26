6067a68a1f05f59554787d56efd28fa65d00c487
Antoine <antoine@biin.fr>
2013-06-26 15:02:17 +0200

    Expo musée d'archéologie

--- app/expos/40/data/__init__.py
+++ app/expos/40/data/__init__.py
@@ -31,27 +31,28 @@ ImageItem.text = StringProperty('')
 ImageItem.english = BooleanProperty(False)
 
 def my_on_start(self):     
-    self.size = self.img_square.size
+    if self.app.expo_id == '40':
+        self.size = self.img_square.size
 
-    if self.english:
-        endict = self.item['english']
-        if 'nom' in endict:
-            self.name = endict['nom']
-        if 'freefield' in endict:
-            self.subtitle = endict['freefield']
-        if 'orig_geo_prec' in endict:
-            self.copyright = endict['orig_geo_prec']
-        if 'description' in endict:
-            self.description = endict['description'].replace('\r', '')
-    else:
-        self.description = self.item.description.replace('\r', '')
-        self.name = self.item['nom']
-        self.subtitle = self.item['freefield']
-        self.copyright = self.item['orig_geo_prec']
-    self.text = self.description + self.name + self.subtitle + self.copyright
+        if self.english:
+            endict = self.item['english']
+            if 'nom' in endict:
+                self.name = endict['nom']
+            if 'freefield' in endict:
+                self.subtitle = endict['freefield']
+            if 'orig_geo_prec' in endict:
+                self.copyright = endict['orig_geo_prec']
+            if 'description' in endict:
+                self.description = endict['description'].replace('\r', '')
+        else:
+            self.description = self.item.description.replace('\r', '')
+            self.name = self.item['nom']
+            self.subtitle = self.item['freefield']
+            self.copyright = self.item['orig_geo_prec']
+        self.text = self.description + self.name + self.subtitle + self.copyright
 
-    self.height += self.text_content_label.height
-    self.y -= self.text_content_label.height
+        self.height += self.text_content_label.height
+        self.y -= self.text_content_label.height
 
 ImageItem.on_start = my_on_start  
 
@@ -98,19 +99,11 @@ class Glass(Scatter):
     children_items = ListProperty([])
     child_thumbs = ListProperty([])
 
-
     def __init__(self, **kwargs):
         super(Glass, self).__init__(**kwargs)
         self.color = (random.random(), random.random(), random.random(), 1)
         self.orig_width = self.width
-        # self.img = Image(source=self.bg_img.source, keep_data=True)
-        # self.coreimage = CoreImage(self.bg_img.source, keep_data=True)
-        # if self.background_img != '':
-        #     self.bg_img = Image(source=self.background_img, size_hint=(None,None), size=self.size, keep_data=True)
-        #     self.add_widget(self.bg_img)
-        # if self.glass_img != '':
-        #     glass_img = Image(source=self.glass_img , size_hint=(None,None), size=self.size, color=self.color)
-        #     self.add_widget(glass_img)
+        self.keywords = self.app.db.keywords[0]["children"]
 
     def remove(self):
         if not self.disabled:
@@ -118,13 +111,25 @@ class Glass(Scatter):
             self.parent.remove_widget(self)
             self.app.glass_count -= 1
 
+    def match_filename_keyword(self, name):
+        for key in self.keywords:
+            if key["name"] == name:
+                return key['id']
+
     def get_item_from_filename(self, filename_id):
+        key_id = self.match_filename_keyword(filename_id)
+        its = []
         for item in self.app.db.items:
-            if item.datation == filename_id:
-                self.item = item
-                self.content = True
-                self.get_children()
-                return
+            if key_id in item.keywords:
+                it = {}
+                it["item"] = item
+                its.append(it)
+
+        if len(its) > 0:
+            it = random.choice(its)
+            self.item = it["item"]
+            self.content = True
+            self.get_children()
 
     def get_children(self):
         children_ids = self.item.children
@@ -305,15 +310,21 @@ def build(app):
             if but.english:
                 x = center_x + 150
                 y = center_y - 150 
+                center_x -= 150
+                center_y += 150
             else:                
                 x = center_x - 150
                 y = center_y + 150 
+                center_x += 150
+                center_y -= 150
             glass = Glass(  center=(x,y),
                             size=(300,300),
                             app= app,
                             english=but.english)
             root.add_widget(glass)
             app.glass_count += 1
+            
+
             anim = Animation(center_x=center_x , center_y=center_y, d=.1, t='out_cubic')
             anim.start(glass)
 
@@ -330,6 +341,7 @@ def build(app):
                     do_translation=False,
                     scale=1,
                     size_hint=(None,None),
+                    rotation=180 ,
                     center=(75, Window.height-75))    
 
     but = Button(   size=(85,85),
@@ -341,9 +353,14 @@ def build(app):
                     # center=(75, Window.height-75))
     scat.add_widget(but)
     root.add_widget(scat)
+    scat.center = (75, Window.height-75)
     but.english = False
 
-    scat2 = Scatter( size=(85,85), 
+    def change_expo(but):
+        app.change_expo(str(41))
+        # app.show_expo(str(41))
+
+    scat2 = Scatter(size=(85,85), 
                     do_scale=False, 
                     do_rotation=False,
                     do_translation=False,
@@ -351,17 +368,53 @@ def build(app):
                     size_hint=(None,None),
                     center=(Window.width - 75, 75))    
 
-    but2 = Button(   size=(85,85),
+    but2 = Button(  size=(85,85),
                     size_hint= (None,None),
                     background_normal='widgets/btn-loupe.png',
                     background_down='widgets/btn-loupe.png',
                     on_press=increase_button,
                     on_release=add_glass)
-                    # center=(75, Window.height-75))
     scat2.add_widget(but2)
     root.add_widget(scat2)
+    scat2.center = (Window.width - 75, 75)
     but2.english = True
 
+    scat3 = Scatter(size=(130,130), 
+                    do_scale=False, 
+                    do_rotation=False,
+                    do_translation=False,
+                    scale=1,
+                    rotation=180,
+                    size_hint=(None,None),
+                    center=(75, 75))    
+
+    but3 = Button(  size=(130,130),
+                    size_hint= (None,None),
+                    background_normal='widgets/btn-switch-mode.png',
+                    background_down='widgets/btn-switch-mode.png',
+                    on_press=increase_button,
+                    on_release=change_expo)
+    scat3.add_widget(but3)
+    root.add_widget(scat3)
+    scat3.center = (75,75)
+
+    scat4 = Scatter(size=(130,130), 
+                    do_scale=False, 
+                    do_rotation=False,
+                    do_translation=False,
+                    scale=1,
+                    size_hint=(None,None),
+                    center=(Window.width -75, Window.height - 75))    
+
+    but4 = Button(  size=(130,130),
+                    size_hint= (None,None),
+                    background_normal='widgets/btn-switch-mode.png',
+                    background_down='widgets/btn-switch-mode.png',
+                    on_press=increase_button,
+                    on_release=change_expo)
+    scat4.add_widget(but4)
+    root.add_widget(scat4)
+    scat4.center = (Window.width -75, Window.height - 75)
 
     def anim_buttons(dt):
         key = random.random()
@@ -372,9 +425,7 @@ def build(app):
             else:
                 increase_button(but)
 
-    Clock.schedule_interval(anim_buttons, 2)
-
-    
+    # Clock.schedule_interval(anim_buttons, 2)
 
 
     root.hide_items = True
