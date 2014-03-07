71b20a633a1bb5ac778816b027c942300c23efa8
Antoine <antoine@biin.fr>
2014-03-07 09:36:09 +0100

    Museolib slider add parameters padding and bound_min to slider

--- app/museolib/widgets/slider.py
+++ app/museolib/widgets/slider.py
@@ -1,13 +1,15 @@
 from kivy.uix.floatlayout import FloatLayout
-from kivy.properties import ReferenceListProperty, NumericProperty, StringProperty
+from kivy.properties import ReferenceListProperty, NumericProperty, StringProperty, ObjectProperty
 
 
 class SizeSlider(FloatLayout):
+    bound_min = NumericProperty(0.0)
     value_min = NumericProperty(0.0)
     value_max = NumericProperty(1.0)
     value_range = ReferenceListProperty(value_min, value_max)
     text_min = StringProperty('')
     text_max = StringProperty('')
+    padding = NumericProperty(0.0)
 
     def on_touch_down(self, touch):
         if not self.collide_point(*touch.pos):
@@ -28,12 +30,15 @@ class SizeSlider(FloatLayout):
             return True
 
     def update_from_touch(self, touch):
-        value = (touch.x - self.x) / self.width
-        value = min(1.0, max(0.0, value))
+        value = (touch.x - self.x + self.padding/2) / (self.width - self.padding)
+        value = min(1.0, max(self.bound_min, value))
         value_center = (self.value_min + self.value_max) / 2.
         name = 'value_min' if value < value_center else 'value_max'
         setattr(self, name, value)
 
+    def reset(self):
+        self.value_min = self.bound_min
+        self.value_max = 1
 
 if __name__ == '__main__':
     from kivy.base import runTouchApp
